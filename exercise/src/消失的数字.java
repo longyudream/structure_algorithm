8978a787a96306e70da3c85705b00bbf5bf33fa4
chenzhenglong <longyu820729@163.com>
2021-04-22 10:26:11 +0800

    数据结构与算法

---
+++ exercise/src/消失的数字.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author 陈正龙
+ * @date 2021/4/22 10:04
+ */
+public class 消失的数字 {
+    public static void main(String[] args) {
+        /***
+         * 面熟 : 数组nums中包含0 到 n中所有的整数 但是其中缺了一个 请找出这个数字 并且时间复杂度为0(N)
+         */
+
+    }
+
+//    static int getLost(int[] nums) {
+//        int length = nums.length;
+//
+//
+//    }
+}
