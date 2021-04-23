c09e9a8cf5078f94c93ac2cca34fc9c61e814165
chenzhenglong <longyu820729@163.com>
2021-04-23 11:30:36 +0800

    消失的数字

--- exercise/src/消失的数字.java
+++ exercise/src/消失的数字.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * @author 陈正龙
@@ -8,14 +9,24 @@ import java.util.Collections;
 public class 消失的数字 {
     public static void main(String[] args) {
         /***
-         * 面熟 : 数组nums中包含0 到 n中所有的整数 但是其中缺了一个 请找出这个数字 并且时间复杂度为0(N)
+         * 描述 : 数组nums中包含0 到 n中所有的整数 但是其中缺了一个 请找出这个数字 并且时间复杂度为0(N)
          */
+        int[] arr= {3,0,2};
+        int lost = getLost(arr);
+        System.out.println("lost = " + lost);
 
     }
 
-//    static int getLost(int[] nums) {
-//        int length = nums.length;
-//
-//
-//    }
+    static int getLost(int[] nums) {
+        int[]temp=  new int[nums.length+1];
+        for (int num : nums) {
+            temp[num]=num;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i]!=i){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
